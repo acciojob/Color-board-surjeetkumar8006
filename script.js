017464035b16a39c6ef95e01697ab731f9a5e12a
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2026-02-05 13:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const container = document.getElementById("container");
 const TOTAL_SQUARES = 800;
 
 const colors = [
-  "#e74c3c",
-  "#8e44ad",
-  "#3498db",
-  "#e67e22",
-  "#2ecc71",
-  "#f1c40f"
+  "rgb(231, 76, 60)",
+  "rgb(142, 68, 173)",
+  "rgb(52, 152, 219)",
+  "rgb(230, 126, 34)",
+  "rgb(46, 204, 113)",
 ];
 
 function getRandomColor() {
@@ -18,12 +17,12 @@ for (let i = 0; i < TOTAL_SQUARES; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  square.addEventListener("mouseenter", () => {
+  square.addEventListener("mouseover", () => {
     square.style.backgroundColor = getRandomColor();
   });
 
-  square.addEventListener("mouseleave", () => {
-    square.style.backgroundColor = "#1d1d1d";
+  square.addEventListener("mouseout", () => {
+    square.style.backgroundColor = "rgb(29, 29, 29)";
   });
 
   container.appendChild(square);
